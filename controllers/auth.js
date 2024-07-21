ea37aa9bd537847cb9a2ceb1442bef2ef4a07d63
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-07-21 15:33:06 -0500

    added authentication script

---
+++ controllers/auth.js
@@ -0,0 +1,101 @@
+const passport = require('passport')
+const validator = require('validator')
+const User = require('../models/User')
+
+//renders the login page if the user is not already authenticated. If the user is authenticated, they are redirected to the '/todos' page. 
+ exports.getLogin = (req, res) => {
+    if (req.user) {
+      return res.redirect('/todos')
+    }
+    res.render('login', {
+      title: 'Login'
+    })
+  }
+  
+  //you'll want to export modules so that you can use them in other parts of your application. This block handles login form submission. Validates the input, autenticates the user using 'passport', and handles any errors. If authentication is successful, the user is logged in and redirected to the '/todos' page. 
+  exports.postLogin = (req, res, next) => {
+    const validationErrors = []
+    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
+    if (validator.isEmpty(req.body.password)) validationErrors.push({ msg: 'Password cannot be blank.' })
+  
+    if (validationErrors.length) {
+      req.flash('errors', validationErrors)
+      return res.redirect('/login')
+    }
+    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
+  
+    passport.authenticate('local', (err, user, info) => {
+      if (err) { return next(err) }
+      if (!user) {
+        req.flash('errors', info)
+        return res.redirect('/login')
+      }
+      req.logIn(user, (err) => {
+        if (err) { return next(err) }
+        req.flash('success', { msg: 'Success! You are logged in.' })
+        res.redirect(req.session.returnTo || '/todos')
+      })
+    })(req, res, next)
+  }
+  
+  //logs the user out, destroys the session, and redirects to the home page. Also logs a message to the console and handles potential errors during session destruction
+  exports.logout = (req, res) => {
+    req.logout(() => {
+      console.log('User has logged out.')
+    })
+    req.session.destroy((err) => {
+      if (err) console.log('Error : Failed to destroy the session during logout.', err)
+      req.user = null
+      res.redirect('/')
+    })
+  }
+  
+  //renders the signup page if the user is not already authenticated. If the user is authenticated, they are redirected to the '/todos' page. 
+  exports.getSignup = (req, res) => {
+    if (req.user) {
+      return res.redirect('/todos')
+    }
+    res.render('signup', {
+      title: 'Create Account'
+    })
+  }
+  
+  //handles signup form submission. Validates the input, checks for existing users, creates a new user, and logs them in if successful. If there are validation errors or the user already exists, it redirects back to the singup page with error messages
+  exports.postSignup = (req, res, next) => {
+    const validationErrors = []
+    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
+    if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
+    if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match' })
+  
+    if (validationErrors.length) {
+      req.flash('errors', validationErrors)
+      return res.redirect('../signup')
+    }
+    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
+  
+    const user = new User({
+      userName: req.body.userName,
+      email: req.body.email,
+      password: req.body.password
+    })
+  
+    User.findOne({$or: [
+      {email: req.body.email},
+      {userName: req.body.userName}
+    ]}, (err, existingUser) => {
+      if (err) { return next(err) }
+      if (existingUser) {
+        req.flash('errors', { msg: 'Account with that email address or username already exists.' })
+        return res.redirect('../signup')
+      }
+      user.save((err) => {
+        if (err) { return next(err) }
+        req.logIn(user, (err) => {
+          if (err) {
+            return next(err)
+          }
+          res.redirect('/todos')
+        })
+      })
+    })
+  }
