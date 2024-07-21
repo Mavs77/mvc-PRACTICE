794c9084a08a37df30392a78624a88b97167767f
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-07-21 16:15:02 -0500

    update again 21

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,6 @@ const authController = require('../controllers/auth')
 const homeController = require('../controllers/home')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-router.get('/', homeController.getIndex) 
 router.get('/', homeController.getIndex)
 router.get('/login', authController.getLogin)
 router.post('/login', authController.postLogin)
